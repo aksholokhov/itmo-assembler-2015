7d52e0990bc681052e41da7112402404cde284d0
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-03-06 23:21:01 +0300

    Worked, almost tested (leaving SPb)

--- Task1/sm/main.cpp
+++ Task1/sm/main.cpp
@@ -7,167 +7,159 @@ using namespace std;
 
 int main() {
     char* buf = new char[20];
-    hw_sprintf(buf, "%d", 50);
+    hw_sprintf(buf, "%+08d", 1005);
     cout<<buf;
     return 0;
 }
 
+const int CONTROL_FLAG = 1;
+const int SIGN_FULL_FLAG = 2;
+const int LEFT_ALIGN_FLAG = 4;
+const int FILL_ZERO_FLAG = 8;
+const int ONLY_MINUS_FLAG = 16;
+const int LONG_FLAG = 32;
+const int SIGNED_NUM_FLAG = 64;
+
+
 void hw_sprintf(char *buf, char const *format, ...) {
+
     va_list params;
     va_start(params, format);
     bool correctSequence = false;
-    char* reservedBuf = buf;
+    char * reservedBuf = buf;
     int flags = 0;
     int mul = 1;
     int width = 0;
     char const *c = format;
     while (*c != 0) {
-        if (!correctSequence) {
-            if (*c != '%') {
-                *buf = *c;
-                buf++;
-                continue;
-            }
-            correctSequence = true;
-            flags = 0;
-            width = 0;
-            mul = 1;
-            c++;
-            continue;
-
-        }
         if (*c == '%') {
-            *buf = *c;
-            buf++;
-            c++;
-            continue;
+            flags ^= CONTROL_FLAG;
         }
-        if (*c == '+') {
-            flags |= 2;
-            c++;
-            continue;
-        }
-        if (*c == '-') {
-            flags |= 4;
-            c++;
-            continue;
-        }
-        if (*c == ' ') {
-            flags |= 16;
-            c++;
-            continue;
-        }
-        if (*c == '0') {
-            if ((flags & 4) != 4) flags |= 8;
-            c++;
-            continue;
-        }
-
-        if ((*c > '0') && (*c <= '9')) {
-            width += mul * (*c - '0');
-            mul *= 10;
-            c++;
-            continue;
-        }
-        if ((*c == 'l') && (format[(*c) + 1] == 'l')) {
-            flags |= 32;
-            c += 1;
-            c++;
-            continue;
-        }
-        if ((*c == 'i') || (*c == 'd')) {
-            flags |= 64;
-            if (flags & 32) {
-                long long int a = va_arg(params, long long int);
-                my_sequence_print(buf, a, flags, width);
-                correctSequence = false;
+        if (flags & CONTROL_FLAG) {
+            if (*c == '+') {
+                flags |= SIGN_FULL_FLAG;
                 c++;
                 continue;
             }
-            int a = va_arg(params, int);
-            cout<<a<<" "<<width<<'\n';
-            my_sequence_print(buf, a, flags, width);
-            correctSequence = false;
-            c++;
-            continue;
-        }
-        if (*c == 'u') {
+            if (*c == '-') {
+                flags |= LEFT_ALIGN_FLAG;
+                c++;
+                continue;
+            }
+            if (*c == ' ') {
+                flags |= ONLY_MINUS_FLAG;
+                c++;
+                continue;
+            }
+            if (*c == '0') {
+                if ((flags & LEFT_ALIGN_FLAG) != LEFT_ALIGN_FLAG) flags |= FILL_ZERO_FLAG;
+                c++;
+                continue;
+            }
 
+            if ((*c > '0') && (*c <= '9')) {
+                width += mul * (*c - '0');
+                mul *= 10;
+                c++;
+                continue;
+            }
+            if ((*c == 'l') && (format[(*c) + 1] == 'l')) {
+                flags |= LONG_FLAG;
+                c += 2;
+                continue;
+            }
+            if ((*c == 'i') || (*c == 'd') || (*c == 'u')) {
+                if ((*c == 'i') || (*c == 'd')) flags |= SIGNED_NUM_FLAG;
+                if ((flags & LONG_FLAG) == LONG_FLAG) {
+                    long long int a = va_arg(params, long long int);
+                    my_sequence_print(buf, a, flags, width);
+                } else {
+                    int a = va_arg(params, int);
+                    my_sequence_print(buf, a, flags, width);
+                }
+                c++;
+                continue;
+            }
+            if (*c != '%') {
+                *buf++ = '%';
+                *buf++ = *c;
+                flags = 0;
+                width = 0;
+            }
+        } else {
+            *buf++ = *c;
         }
-        *buf = *c;
-        buf++;
-        c++;
-        correctSequence = false;
+        c++; // is better than c, for sure
     }
-    *buf = '0';
-    buf = reservedBuf;
-
+    *buf++ = 0;
 }
 
-void my_sequence_print(char *buf, long long int c, int flags, int width){
+void my_sequence_print(char *& buf, long long int c, int flags, int width){
 
 }
 
 void my_sequence_print(char *& buf, int c, int flags, int width) {
-    if (width == 0) {
-        if (c >= 0) {
-            if ((flags & 2) == 2) {
-                *buf = '+';
-                buf++;
-                if ((flags & 16) == 16) {
-                    *buf = ' ';
-                    buf++;
-                }
-            }
-        }
-        my_itoa(c, buf);
-        buf++;
-        return;
-    }
+
     int t = c;
     int i = 0;
-    if (t < 0) i += 1;
+    if ((t < 0) || ((flags & SIGNED_NUM_FLAG) == SIGNED_NUM_FLAG)) i += 1;
     while (t != 0) {
         t /= 10;
         i++;
     }
     t = width;
     t -= i;
-    if ((flags & 4) == 4) {
-        itoa(c, buf, 10);
-        for (int j = 0; j < t; j++) {
-            *buf = ' ';
-            buf++;
-        }
-        return;
-    }
 
-    for (int j = 0; j < t; j++) {
-        if ((flags & 8) == 8) {
-            *buf = '0';
+    if (t <= 0) {
+        print_num(buf, c, flags);
+    } else  {
+        if ((flags & LEFT_ALIGN_FLAG) == LEFT_ALIGN_FLAG) {
+            print_num(buf, c, flags);
         }
-        else {
-            *buf = ' ';
+        //TODO: 000+1005
+        for (int j = 0; j < t; j++) {
+            if ((flags & FILL_ZERO_FLAG) == FILL_ZERO_FLAG) *buf++ = '0';
+            else *buf++ = ' ';
         }
-        buf++;
+        if ((flags & LEFT_ALIGN_FLAG) == 0) {
+            print_num(buf, c, flags);
+        }
     }
-    my_itoa(c, buf);
+}
+
+void print_num(char *& buf, int c, int flags) {
+        if ((flags & SIGNED_NUM_FLAG) == SIGNED_NUM_FLAG) {
+            if (c < 0) {
+                *buf++ = '-';
+                c = -c;
+            } else  {
+                if ((flags & SIGN_FULL_FLAG) == SIGN_FULL_FLAG) {
+                    *buf++ = '+';
+                }
+                else if ((flags & ONLY_MINUS_FLAG) == ONLY_MINUS_FLAG) {
+                    *buf++ = ' ';
+                }
+            }
+        }
+        my_itoa(c, buf);
 }
 
 void my_itoa(int a, char *& buf) {
     int i = 0;
-    char* buf2 = new char[5];
+    char* buf2 = new char[20];
     while (a != 0) {
         int q = a%10;
-        cout<<q<<'\n';
+        *buf2++ = '0' + q;
         i++;
-        buf2++;
         a/=10;
     }
+    buf2--;
+    cout<<i<<'\n';
     while (i != 0) {
-        buf2--;
-        *buf == *buf2;
-        buf++;
+        cout<<*buf2<<' ';
+        // Lesha lalka
+        *buf++ =/*=*/ *buf2--;
         i--;
     }
+    cout<<'\n';
 }
--- Task1/sm/main.h
+++ Task1/sm/main.h
@@ -1,4 +1,5 @@
 void hw_sprintf(char *buf, char const *format ...);
-void my_sequence_print(char *buf, long long int c, int flags, int width);
+void my_sequence_print(char *& buf, long long int c, int flags, int width);
 void my_sequence_print(char *& buf, int c, int flags, int width);
 void my_itoa(int a, char *& buf);
+void print_num(char *& buf, int c, int flags);

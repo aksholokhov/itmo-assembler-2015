ed41d7ec0eec6121e8f8644d0be2cfee986143ef
Alex Sholokhov <ak.sholokhov@gmail.com>
2015-03-07 00:18:32 +0300

    HW1 tests worked. LL print still empty

--- Task1/sm/main.cpp
+++ Task1/sm/main.cpp
@@ -6,9 +6,21 @@
 using namespace std;
 
 int main() {
-    char* buf = new char[20];
-    hw_sprintf(buf, "%+08d", 1005);
-    cout<<buf;
+    char* out = new char[20];
+    hw_sprintf(out, "Hello world %d\n", 239);
+    cout<<out;
+    hw_sprintf(out, "%0+5u\n", 51);
+    cout<<out;
+    hw_sprintf(out, "<%8u=%-8u>\n", 1234, 5678);
+    cout<<out;
+    hw_sprintf(out, "%i\n", -1);
+    cout<<out;
+    //hw_sprintf(out, "%llu\n", (long long)-1);
+    //cout<<out<<'\n';
+    hw_sprintf(out, "%wtf\n", 1, 2, 3, 4);
+    cout<<out;
+    hw_sprintf(out, "50%%\n");
+    cout<<out;
     return 0;
 }
 
@@ -35,7 +47,7 @@ void hw_sprintf(char *buf, char const *format, ...) {
         if (*c == '%') {
             flags ^= CONTROL_FLAG;
         }
-        if (flags & CONTROL_FLAG) {
+        if ((flags & CONTROL_FLAG) == CONTROL_FLAG) {
             if (*c == '+') {
                 flags |= SIGN_FULL_FLAG;
                 c++;
@@ -78,13 +90,19 @@ void hw_sprintf(char *buf, char const *format, ...) {
                     my_sequence_print(buf, a, flags, width);
                 }
                 c++;
+                flags = 0;
+                width = 0;
+                mul = 1;
                 continue;
             }
             if (*c != '%') {
                 *buf++ = '%';
                 *buf++ = *c;
+                c++;
                 flags = 0;
                 width = 0;
+                mul = 1;
+                continue;
             }
         } else {
             *buf++ = *c;
@@ -102,32 +120,46 @@ void my_sequence_print(char *& buf, int c, int flags, int width) {
 
     int t = c;
     int i = 0;
-    if ((t < 0) || ((flags & SIGNED_NUM_FLAG) == SIGNED_NUM_FLAG)) i += 1;
+    if ((t < 0) || ((flags & SIGNED_NUM_FLAG) == SIGNED_NUM_FLAG)) {
+            i += 1;
+    }
     while (t != 0) {
         t /= 10;
         i++;
     }
     t = width;
     t -= i;
-
     if (t <= 0) {
-        print_num(buf, c, flags);
+        print_sign(buf, c, flags);
+        if (c < 0) c = -c;
+        print_module(c, buf);
     } else  {
         if ((flags & LEFT_ALIGN_FLAG) == LEFT_ALIGN_FLAG) {
-            print_num(buf, c, flags);
+            print_sign(buf, c, flags);
+            print_module(c, buf);
         }
         //TODO: 000+1005
-        for (int j = 0; j < t; j++) {
-            if ((flags & FILL_ZERO_FLAG) == FILL_ZERO_FLAG) *buf++ = '0';
-            else *buf++ = ' ';
+
+        if ((flags & FILL_ZERO_FLAG) == 0) {
+            for (int j = 0; j < t; j++) {
+                *buf++ = ' ';
+            }
         }
+        print_sign(buf, c, flags);
+
+        if ((flags & FILL_ZERO_FLAG) == FILL_ZERO_FLAG) {
+            for (int j = 0; j < t; j++) {
+                *buf++ = '0';
+            }
+        }
+
         if ((flags & LEFT_ALIGN_FLAG) == 0) {
-            print_num(buf, c, flags);
+            print_module(c, buf);
         }
     }
 }
 
-void print_num(char *& buf, int c, int flags) {
+void print_sign(char *& buf, int c, int flags) {
         if ((flags & SIGNED_NUM_FLAG) == SIGNED_NUM_FLAG) {
             if (c < 0) {
                 *buf++ = '-';
@@ -141,10 +173,9 @@ void print_num(char *& buf, int c, int flags) {
                 }
             }
         }
-        my_itoa(c, buf);
 }
 
-void my_itoa(int a, char *& buf) {
+void print_module(int a, char *& buf) {
     int i = 0;
     char* buf2 = new char[20];
     while (a != 0) {
@@ -154,12 +185,8 @@ void my_itoa(int a, char *& buf) {
         a/=10;
     }
     buf2--;
-    cout<<i<<'\n';
     while (i != 0) {
-        cout<<*buf2<<' ';
-        // Lesha lalka
-        *buf++ =/*=*/ *buf2--;
+        *buf++ = *buf2--;
         i--;
     }
-    cout<<'\n';
 }
